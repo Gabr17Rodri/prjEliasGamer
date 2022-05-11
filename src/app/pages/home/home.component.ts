c5f386b45a5944c4b10f0826de9e3f757e727e95
unknown <aluno@etec.sp.gov.br>
2022-05-10 22:41:21 -0300

    aula10/05 criacao da home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  produtos: string[] = [
+    'Produto 1',
+    'Produto 2',
+    'Produto 3',
+    'Produto 4',
+    'Produto 5',
+    'Produto 6',
+  ];
 
   constructor() { }
 
