4e0cda5fc235c9ef7520f8dc1744f97cb0a53ff3
unknown <eu.gabriel.araujo.rodrigues@gmail.com>
2022-05-13 21:24:53 -0300

    Criado as paginas de login e catalogo

---
+++ src/app/caixinha-edicao/caixinha-edicao.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caixinha-edicao',
+  templateUrl: './caixinha-edicao.component.html',
+  styleUrls: ['./caixinha-edicao.component.css']
+})
+export class CaixinhaEdicaoComponent implements OnInit {
+  @Input() nomeProduto: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
